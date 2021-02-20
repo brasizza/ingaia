a5dda04b32f2f0424d86ad067a70a0b184892076
Marcus Brasizza <marcusbrasizza@MacBook-Pro-de-Marcus.local>
2021-02-20 14:15:47 -0300

    ingaia_final

--- app/Http/Controllers/IngaiaController.php
+++ app/Http/Controllers/IngaiaController.php
@@ -34,7 +34,11 @@ class IngaiaController extends Controller
 
         if($getXML){
             $xml = (array) simplexml_load_string($getXML,"SimpleXMLElement",LIBXML_NOCDATA);
-            return $this->successResponse( json_encode($xml, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES));
+            if($xml){
+                return $this->successResponse( json_encode($xml, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES));
+            }else{
+                return $this->errorResponse("Falha ao recuperar os dados do XML", Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
 
         }else{
             return $this->errorResponse("Falha ao recuperar dados da url :" . $urlGuzzle, Response::HTTP_BAD_REQUEST);
