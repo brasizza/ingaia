0c797b19a83f59b018cb6aa3edc05db14c274a8f
Marcus Brasizza <marcusbrasizza@MacBook-Pro-de-Marcus.local>
2021-02-20 14:14:43 -0300

    WEBSERVICE --->> XML to JSON

---
+++ app/Http/Controllers/IngaiaController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\GuzzleHelper;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class IngaiaController extends Controller
+{
+    use ApiResponser;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function processXML(Request $request){
+
+
+        $url =   implode('&', ($request->all()));
+        $parameter =  urlencode($request->p);
+
+
+        $urlGuzzle = $url.'&p='.$parameter;
+
+
+        $getXML = GuzzleHelper::get($urlGuzzle,20);
+
+        if($getXML){
+            $xml = (array) simplexml_load_string($getXML,"SimpleXMLElement",LIBXML_NOCDATA);
+            return $this->successResponse( json_encode($xml, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES));
+
+        }else{
+            return $this->errorResponse("Falha ao recuperar dados da url :" . $urlGuzzle, Response::HTTP_BAD_REQUEST);
+        }
+
+    }
+
+
+    //
+}
